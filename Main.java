0de890d85530be4627f5fb9309eb8ba52ad0ac0b
JakubCzyrek <101180591+JakubCzyrek@users.noreply.github.com>
2022-03-22 10:06:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int x =45;
+    char y = 'b';
+    double z = -95.11;
+    byte a = 12;
+
+    System.out.println("int ="+x);
+    System.out.println("char ="+y);
+    System.out.println("double ="+z);
+    System.out.println("byte ="+a);   
+  }
+}
